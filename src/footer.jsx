d65aaad66e1c0bde4ac646ff8e03f79f68f1f867
ArchetypeLegion <122025812+ArchetypeLegion@users.noreply.github.com>
2024-04-03 16:16:10 +0300

    first commit

--- src/footer.jsx
+++ src/footer.jsx
@@ -4,20 +4,20 @@ function Footer(){
     return(
         <div id="footer" className = "footer"> 
             <div class="adress"> 
-                <img id = "logoend" src="../vite-project/public/logo.png" alt="logo"/>
+                <img id = "logoend" src="./public/logo.png" alt="logo"/>
                 <h1>Charlotte Street Hotel   </h1>
                 <p>193 Victoria Street London EC35 4LK</p>
             </div>
             <div class="contact">
-                <img class="contactimg" src="../vite-project/public/contacts.png" alt="contacts"/>    
+                <img class="contactimg" src="./public/contacts.png" alt="contacts"/>    
                 <p>+4 417-688-18-56</p> 
                 <p>HotelLigma@gmail.com</p>
             </div>
             <div class="follow">
                 <h2>Следуйте за нами в Instagram</h2>
-                <a href="#"><img src="../vite-project/public/instagram.png" alt="instagram"/></a>
-                <a href="#"><img src="../vite-project/public/facebook.png" alt="facebook"/></a>
-                <a href="#"><img src="../vite-project/public/telegram.png" alt="telegram"/></a>
+                <a href="#"><img src="./public/instagram.png" alt="instagram"/></a>
+                <a href="#"><img src="./public/facebook.png" alt="facebook"/></a>
+                <a href="#"><img src="./public/telegram.png" alt="telegram"/></a>
             </div>
     </div>
     );
--- src/slider.jsx
+++ src/slider.jsx
@@ -7,14 +7,14 @@ function Slider(){
         <div className="slide_container">
          <div className = "slides">
             <div className="slide">
-             <img src="../vite-project/public/room1.jpg" alt="slide1"/></div>
+             <img src="../public/room1.jpg" alt="slide1"/></div>
             <div className="slide">
-                <img src="../vite-project/public/bathroom1.jpg" alt="slide2"/></div>
+                <img src="../public/bathroom1.jpg" alt="slide2"/></div>
             <div className="slide">
-             <img src="../vite-project/public/kitchen1.jpg" alt="slide3"/></div>
+             <img src="../public/kitchen1.jpg" alt="slide3"/></div>
         </div>
-            <button id="prevBtn" className="prevBtn"><img src="../vite-project/public/left-arrow.png" alt="left"/></button>
-            <button id="nextBtn" className="nextBtn"><img src="../vite-project/public/right-arrow.png" alt="right"/></button>
+            <button id="prevBtn" className="prevBtn"><img src="./public/left-arrow.png" alt="left"/></button>
+            <button id="nextBtn" className="nextBtn"><img src="./public/right-arrow.png" alt="right"/></button>
         </div>
         <script src="../src/js.js"></script>
         </React.Fragment>
