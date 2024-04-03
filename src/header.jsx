8f413cd198eb75e190f06ce9fc0b3724f918b4ed
ArchetypeLegion <122025812+ArchetypeLegion@users.noreply.github.com>
2024-04-03 20:53:16 +0300

    first commit

--- src/header.jsx
+++ src/header.jsx
@@ -4,7 +4,7 @@ import './header.css'
 function Header(){
     return(
         <header>
-            <img src="https://hsygvgnykngkzvna.public.blob.vercel-storage.com/logo-XbOxitXNNSrdB9NNaFtXMe30aC1xrz.png" alt="logo" />
+            <img id = "logo" src="https://hsygvgnykngkzvna.public.blob.vercel-storage.com/logo-XbOxitXNNSrdB9NNaFtXMe30aC1xrz.png" alt="logo" />
             <h1 className="maintext">Hotel Luxurious International Getaway Magnificent Abode</h1>
             <nav className="head_menu">
             <ul>
